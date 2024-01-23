1bf5f92d2bae1fcf719fb3013a89c300a7767c6d
Tsolak <dcntsolak@yahoo.com>
2024-01-23 15:23:20 +0400

    Solved Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Задайте массив заполненный
+//случайными трёхзначными числами.
+//Напишите программу, которая покажет
+//количество чётных чисел в массиве.
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем размер массива
+        int[] array = new int[10];
+        Random rnd = new Random();
+        int counter = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(100, 1000);
+            Console.WriteLine($"Трехзначное число: {array[i]}");
+            foreach (int e in array)
+        {
+            if (e % 2 == 0)
+            {
+                counter++;
+            }
+        }
+            Console.WriteLine($"Количество четных чисел: {counter}");
+        }
+    }
+}       
+
+   
+        
