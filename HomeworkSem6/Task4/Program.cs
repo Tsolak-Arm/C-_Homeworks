b34cfe168b2dbc157821c76b8b379c4e93d7fefd
Tsolak <dcntsolak@yahoo.com>
2024-01-29 15:59:50 +0400

    Solved HomeworkSem6:Task4

---
+++ HomeworkSem6/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами.
+//Сформировать строку, в которой слова расположены в обратном порядке.
+//В полученной строке слова должны быть также разделены пробелами.
+
+Console.WriteLine("Input a string with spaces: ");
+string input = Console.ReadLine();
+
+    if (input != null)
+        {
+            string reversedString = ReverseWords(input);
+            Console.WriteLine("Результат: " + reversedString);
+        }
+    else
+        {
+            Console.WriteLine("Введена пустая строка.");
+        }
+
+    string ReverseWords(string input)
+    {
+        
+        string[] words = input.Split(' ');
+        string[] reversedWords = new string[words.Length];
+            for (int i = 0; i < words.Length; i++)
+            {
+            reversedWords[i] = words[words.Length - 1 - i];
+            }
+        string reversedString = string.Join(" ", reversedWords);
+
+        return reversedString;
+    }
