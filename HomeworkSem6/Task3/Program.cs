f1624fccf55d89d3ef80317ec57fd763853bfdfe
Tsolak <dcntsolak@yahoo.com>
2024-01-29 15:46:31 +0400

    Solved Homework6:Task3

---
+++ HomeworkSem6/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 3: Задайте произвольную строку.
+//Выясните, является ли она палиндромом.
+
+Console.WriteLine("Введите строку для проверки:");
+    string input = Console.ReadLine();
+
+bool IsPalindrome(string str)
+    {
+        str = str.Replace(" ", "").ToLower();
+        for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+        {
+            if (str[i] != str[j])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+void PrintIsPalindrome(string str)
+    {
+        if (IsPalindrome(str))
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+
+    PrintIsPalindrome(input);
+    
+
+   
