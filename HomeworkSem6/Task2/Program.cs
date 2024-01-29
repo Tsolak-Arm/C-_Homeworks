67a404347a7567d02f0e25fb20e0b101041f3bb3
Tsolak <dcntsolak@yahoo.com>
2024-01-29 14:51:22 +0400

    Solved HomeworkSem6:Task2

---
+++ HomeworkSem6/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах.
+//Сформируйте строку, в которой все заглавные буквы заменены на строчные. 
+    
+
+    string ConvertToLowerCase(string str)
+    {
+        char[] chars = str.ToCharArray();
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            // Проверяем, является ли символ заглавной буквой
+            if (chars[i] >= 'A' && chars[i] <= 'Z')
+            {
+                // Вычитаем разницу между кодами заглавной и строчной буквы,
+                // чтобы получить символ в строчном регистре
+                chars[i] = (char)(chars[i] - 'A' + 'a');
+            }
+        }
+
+        return new string(chars);
+    }
+    string originalString = "AbcD15FplO";
+    string lowerCaseString = ConvertToLowerCase(originalString);
+
+        Console.WriteLine($"Исходная строка: {originalString}");
+        Console.WriteLine($"Строка с заменой заглавных на строчные: {lowerCaseString}");
