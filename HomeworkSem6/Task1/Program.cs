9f4c950d70417ef3c6187f055172e959655eabbc
Tsolak <dcntsolak@yahoo.com>
2024-01-29 11:31:29 +0400

    Solved HomeworkSem6:Task1

---
+++ HomeworkSem6/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 1: Задайте двумерный массив символов (тип char [,]).
+//Создать строку из символов этого массива.
+
+string MatrixToString(char[,] chars)
+{
+    string str = "";
+    int rows = chars.GetLength(0);
+    int columns = chars.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                str += chars[i, j];
+            }
+        }
+        return str;
+}
+char[,] chars = {
+            {'a', 'b', 'c'},
+            {'d', 'e', 'f'}
+        };
+string result = MatrixToString(chars);
+Console.WriteLine($"'{result}'");
